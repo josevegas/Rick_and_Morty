a8ad99ab30cc05e8cfb5366332e6aac0d48e2b51
Jose Vegas <josevegas.marquez@gmail.com>
2023-03-03 01:11:01 -0500

    avance

--- rick_and_morty/src/components/Card.jsx
+++ rick_and_morty/src/components/Card.jsx
@@ -3,12 +3,12 @@ export default function Card(props) {
    return (
       <div className={style.divContent}>
          <div className={style.divCard}>
-         <button onClick={props.onClose}>X</button>
-         <h2>{props.name}</h2>
-         <h2>{props.species}</h2>
-         <h2>{props.gender}</h2>
-         <img  src={props.image} alt="Card" />
-      </div>
+            <button onClick={props.onClose}>X</button>
+            <img  src={props.image} alt="Card" />
+            <h2>{props.name}</h2>
+            <h2>{props.species}</h2>
+            <h2>{props.gender}</h2>
+         </div>
       </div>
    );
 }
