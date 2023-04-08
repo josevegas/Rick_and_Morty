57e6427135d1ed0e2b52f41618db5f96366caa34
Jose Vegas <josevegas.marquez@gmail.com>
2023-04-08 08:50:58 -0500

    mejorando el server

--- rick_and_morty/Server/controllers/getCharById.js
+++ rick_and_morty/Server/controllers/getCharById.js
@@ -1,9 +1,23 @@
 const axios=require('axios');
 
-constgetCharById=(res,id)=>{
-    axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-    .then((res)=>console.log(res.data))
-    .then(data=>{
-        const char={id:data.id,name:data.name,gender:data.gender,species:data.species,origin:data.origin,image:data.image,status:data.status}
-    })
+const getCharById=(res,id)=>{
+    try{
+        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+        .then((response)=>console.log(response.data))
+        .then(data=>{
+            const char={id:data.id,name:data.name,gender:data.gender,species:data.species,origin:data.origin,image:data.image,status:data.status}
+            if(char){
+                res.writeHead(200,{"Content-Type":"applicatioin/json"})
+                res.end(JSON.stringify(char))
+            }else{
+                res.writeHead(500,{"Content-Type":"text/plain"})
+                res.end(JSON.stringify({error:`Character not found with id: ${id}`}))
+            }
+        })
+    }catch(error){
+        console.log(error);
+        throw new Error(error);
+    }
 }
+
+module.exports={getCharById};
--- rick_and_morty/Server/index.js
+++ rick_and_morty/Server/index.js
@@ -1,5 +1,5 @@
 const http=require('http');
-const data=require('./utils/data');
+const getCharById=require('./controllers/getCharById');
 
 const PORT=3001;
 http.createServer((req,res)=>{
@@ -7,15 +7,9 @@ http.createServer((req,res)=>{
     try {
         const{url}=req;
         if(url.includes('/rickandmorty/character')){
-            const id=url.split('/').pop();
-            const character=data.find((char)=>char.id===Number(id))
-            if(character){
-                res.writeHead(200,{"Content-Type":"applicatioin/json"})
-                res.end(JSON.stringify(character))
-            }else{
-                res.writeHead(404,{"Content-Type":"application/json"})
-                res.end(JSON.stringify({error:`Character not found with id: ${id}`}));
-            }
+            const id=Number(url.split('/').pop());
+            getCharById(res,id)
+            
         }
     } catch (error) {
         console.log(error);
