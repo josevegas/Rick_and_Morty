22e424a8430d3e57fc727a117a294f67b11a7f77
josevegas <36419038+josevegas@users.noreply.github.com>
2023-04-12 18:53:45 -0500

    retomando

--- rick_and_morty/Server/controllers/getCharById.js
+++ rick_and_morty/Server/controllers/getCharById.js
@@ -1,23 +1,18 @@
 const axios=require('axios');
 
-const getCharById=(res,id)=>{
-    try{
+function getCharById(res,id){
+    // console.log("llegó")
+    // try{
         axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-        .then((response)=>console.log(response.data))
-        .then(data=>{
-            const char={id:data.id,name:data.name,gender:data.gender,species:data.species,origin:data.origin,image:data.image,status:data.status}
-            if(char){
-                res.writeHead(200,{"Content-Type":"applicatioin/json"})
-                res.end(JSON.stringify(char))
-            }else{
-                res.writeHead(500,{"Content-Type":"text/plain"})
-                res.end(JSON.stringify({error:`Character not found with id: ${id}`}))
-            }
+        .then(response=>{
+                const {id,name,gender,species,origin,image,status}=response.data;
+                res.writeHead(200,{'Content-Type':'application/json'});
+                res.end(JSON.stringify({id,name,gender,species,origin,image,status}))
+            })
+        .catch(reason=>{
+            res.writeHead(500,{'Content-Type':'text/plain'})
+            res.end(reason.message)
         })
-    }catch(error){
-        console.log(error);
-        throw new Error(error);
-    }
 }
 
-module.exports={getCharById};
+module.exports=getCharById;
--- rick_and_morty/Server/index.js
+++ rick_and_morty/Server/index.js
@@ -1,5 +1,6 @@
 const http=require('http');
 const getCharById=require('./controllers/getCharById');
+const axios=require('axios')
 
 const PORT=3001;
 http.createServer((req,res)=>{
@@ -8,8 +9,7 @@ http.createServer((req,res)=>{
         const{url}=req;
         if(url.includes('/rickandmorty/character')){
             const id=Number(url.split('/').pop());
-            getCharById(res,id)
-            
+            getCharById(res,id);
         }
     } catch (error) {
         console.log(error);
