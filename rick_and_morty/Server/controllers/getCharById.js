fc4c89d7f6b8a9050852de9b3d2cbb6c764aab0b
josevegas <36419038+josevegas@users.noreply.github.com>
2023-04-13 18:19:32 -0500

    avanzando

--- rick_and_morty/Server/controllers/getCharById.js
+++ rick_and_morty/Server/controllers/getCharById.js
@@ -1,18 +1,27 @@
 const axios=require('axios');
+const URL="https://rickandmortyapi.com/api/character/"
 
-function getCharById(res,id){
-    // console.log("llegó")
-    // try{
-        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(response=>{
-                const {id,name,gender,species,origin,image,status}=response.data;
-                res.writeHead(200,{'Content-Type':'application/json'});
-                res.end(JSON.stringify({id,name,gender,species,origin,image,status}))
+const getCharById=(req,res)=>{
+    try {
+        const{url}=req;
+        if(url.includes('/rickandmorty/character')){
+            const id=url.split('/').pop();
+            axios.get(URL+id)
+            .then(response=>{
+                    const {id,name,gender,species,origin,image,status}=response.data;
+                    res.writeHead(200,{'Content-Type':'application/json'});
+                    res.end(JSON.stringify({id,name,gender,species,origin,image,status}))
+                })
+            .catch(reason=>{
+                res.writeHead(500,{'Content-Type':'text/plain'})
+                res.end(reason.message)
             })
-        .catch(reason=>{
-            res.writeHead(500,{'Content-Type':'text/plain'})
-            res.end(reason.message)
-        })
+        }
+    }catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
 }
 
+getCharById({url:'/rickandmorty/character/1'})
 module.exports=getCharById;
