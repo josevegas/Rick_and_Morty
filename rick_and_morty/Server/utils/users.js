b839cb5289029120a12b4a3da8d38a1b19cad795
josevegas <36419038+josevegas@users.noreply.github.com>
2023-04-15 08:00:26 -0500

    configurando serer

--- rick_and_morty/Server/utils/users.js
+++ rick_and_morty/Server/utils/users.js
@@ -1,3 +1,4 @@
 require('dotenv').config();
-const users=[{userName:'josevegas@gmail.com',password:process.env.PASSWORD}];
+const {PASSWORD}=process.env;
+const users=[{userName:'josevegas@gmail.com',password:PASSWORD}];
 module.exports=users;
